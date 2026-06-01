313874f13c90241fc8bdf498370e7b9adacd8898
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-06-01 09:01:25 -0300

    Atividade - atividade de php realizada

--- index.php
+++ index.php
@@ -1,82 +1,59 @@
 <?php
-session_start();
 
-$host = "localhost";
-$user = "root";
-$pass = "root";
-$db = "sistema_lucas_onofre_";
+include("infra/db/connect.php");
 
-$conn = new mysqli($host, $user, $pass, $db);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-if ($conn->connect_error) {
-    die("Erro na conexão:");
-}else{
-    echo ("<p> BD: ok </p>");
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
-    
-    $sql = "SELECT * FROM usuarios
+
+    $sql = "SELECT * FROM usuarios 
     WHERE usuario = '$usuario' 
-     AND senha = '$senha'";
+    AND senha = '$senha'";
 
-    $result = $conn-> query($sql);
-}
-    if ($result->num_rows > 0) {
-       $_SESSION["usuario"] = $usuario;
+    $resultado = $conn -> query($sql);
 
-       header("Location: public/home.php");
-       exit();  
-    } else {
-        echo "Usuário ou senha inválidos.";
-
+    if($resultado -> num_rows > 0){
+        $_SESSION["usuario"] = $usuario;
+        header("Location: public/home.php");
+        exit();
+    }else{
+        $erro = "Usuário ou senha inválidos.";
     }
+}
+
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login.php</title>
+    <title>Login com PHP</title>
 </head>
 <body>
-    <h2>Login com PHP</h2>
-
-
-    <form method="POST">
-        <label for="usuario">Usuário:</label>
-        <input type="text" id="usuario" name="usuario">
+     <?php
+    include("public/component/table.php");
+    ?>
+    <h2>Login com PHP</h2><form method="POST">
+        <label for="usuario">Usuario:</label>
+        <input type="text" name="usuario">
         <br>
         <br>
         <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha">
+        <input type="senha" name="senha">
         <br>
         <br>
+        <?php
+
+            if(isset($erro)){
+                echo $erro;
+            }
+        ?>
         <button type="submit">Entrar</button>
     </form>
-    <?php
-   if(isset($erro)){
-     echo $erro;
+    
 
-   }
 
-    ?>
+    
 </body>
 </html>
---
+++ infra/db/connect.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db = "sistema_lucas_onofre_";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Erro na conexão:");
+}else{
+    echo ("<p> BD: ok </p>");
+}
+
+?>
