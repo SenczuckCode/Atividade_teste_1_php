515a5189b055efad2d76a9ff2234160d4fcf4bfc
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-06-03 09:30:17 -0300

    feat - excluir usuário tive dificuldades em realizar a atividade e o sistema ainda apresenta problemas porém o método que pensei foi utilizar html para pegar o id depois utilizar php para excluir o banco do usuário

--- public/home.php
+++ public/home.php
@@ -4,10 +4,12 @@
         header("Location: ../index.php");
         exit();
     }
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+     
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["usuario"])){
         $usuario = $_POST["usuario"];
         $senha = $_POST["senha"];
-        $sql = "INSERT INTO usuarios (usuario, senha) VALUES ('$usuario','$senha')";
+        $sql = "INSERT INTO usuarios (usuario, senha) VALUES ('$$usuario','$$senha')";
         if($conn -> query($sql) === TRUE){
             echo "<script>alert('Usuário Cadastrado com sucesso!')</script>";
         }else{
@@ -43,12 +45,38 @@
         <br>
         <br>
         <button type="submit">Cadastrar</button>
+        <br>
+        <br>    
+        <br>
     </form>
+
+
+    <form id="id_excluir" method="POST">
+        <label for="excluir_conta">Digite o id para excluir a conta</label>
+        <br>
+        <input type="number" name="id_excluir" id="id">
+        <br>
+    <button type="submit">Excluir</button>
+    </form>
+
     <?php
     
-    include("../public/component/table.php");
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id_excluir'])) {
+        $id = $_POST['id_excluir'];
+        $sql = "DELETE FROM usuarios WHERE id = $id";
+        if ($conn->query($sql) === TRUE) {
+        echo "Registro deletado com sucesso!";
+        } else {
+        echo "Erro ao deletar: " . $conn->error;
+        }
+        $conn->close();
+    }
     ?>
 
+
+    <?php
+    include("../public/component/table.php");
+    ?>
 
     <a href="logout.php">Sair</a>
     
